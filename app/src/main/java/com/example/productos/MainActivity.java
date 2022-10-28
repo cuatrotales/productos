93903436a95f079fdc522f476bac5116f1c12012
cuatrotales <jfquinterogu14@gmail.com>
2022-10-27 19:38:39 -0500

    Finalizacion de la aplicacion incluyendo api funcional y a falta de ensayos para la terminacion.

--- app/src/main/java/com/example/productos/MainActivity.java
+++ app/src/main/java/com/example/productos/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.productos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +15,16 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TimerTask splash = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, formProductos.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        Timer tiempo = new Timer();
+        tiempo.schedule(splash, 5000);
     }
 }
--- app/src/main/java/com/example/productos/formProductos.java
+++ app/src/main/java/com/example/productos/formProductos.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -14,17 +15,24 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class formProductos extends Activity implements Response.Listener<JSONObject>, Response.ErrorListener {
 
     EditText jetId, jetNombre, jetValor;
     CheckBox jcbActivo;
-    String Id, Nombre, Valor, Url="http://192.168.20.48:80/WebServices/";
+    String Id,
+            Nombre,
+            Valor,
+            Url="http://192.168.20.48:80/webServices/";
     RequestQueue rq;
     JsonRequest jrq;
 
@@ -39,15 +47,19 @@ public class formProductos extends Activity implements Response.Listener<JSONObj
 
         rq = Volley.newRequestQueue(this);
     }
-    public void Select(){
+    boolean buscarId(){
+        Url+="select.php?id="+Id;
+        jrq = new JsonObjectRequest(Request.Method.GET, Url, null, this, this);
+        rq.add(jrq);
+        return true;
+    }
+    public void select(){
         Id = jetId.getText().toString();
         if (Id.isEmpty()){
             Toast.makeText(this, "¿Sin el ID? Ni con el viento de la rosa de guadalupe...", Toast.LENGTH_SHORT).show();
             jetId.requestFocus();
         }else{
-            Url+="select.php?id="+Id;
-            jrq = new JsonObjectRequest(Request.Method.GET, Url, null, this, this);
-            rq.add(jrq);
+            buscarId();
         }
     }
 
@@ -74,5 +86,109 @@ public class formProductos extends Activity implements Response.Listener<JSONObj
     @Override
     public void onErrorResponse(VolleyError error) {
         Toast.makeText(this, "El producto no se encuentra en nuestra base de datos :c", Toast.LENGTH_SHORT).show();
+    }
+
+    public void insert(View v){
+        Id=jetId.getText().toString();
+        Nombre=jetNombre.getText().toString();
+        Valor=jetValor.getText().toString();
+        if (Id.isEmpty() || Nombre.isEmpty() || Valor.isEmpty()){
+            Toast.makeText(this, "¿Eso vacio? Don chimbilin...", Toast.LENGTH_SHORT).show();
+            jetId.requestFocus();
+        }else{
+            if (buscarId()){
+                Url+="update.php";
+            }else{
+                Url+="insert.php";
+            }
+            StringRequest postRequest = new StringRequest(Request.Method.POST, Url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    limpiarCampos(v);
+                    Toast.makeText(getApplicationContext(), "¿Que pongo?", Toast.LENGTH_SHORT).show();
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(getApplicationContext(), "Nose que poner", Toast.LENGTH_SHORT).show();
+                }
+            }){
+                protected Map<String, String> getParams(){
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("id", jetId.getText().toString().trim());
+                    params.put("nombre", jetNombre.getText().toString().trim());
+                    params.put("valor", jetValor.getText().toString().trim());
+                    return params;
+                }
+            };
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            requestQueue.add((postRequest));
+        }
+    }
+    public void  delete(View v){
+        Id=jetId.getText().toString();
+        if (Id.isEmpty()){
+            Toast.makeText(this, "Debe tener el id, ahuevado", Toast.LENGTH_SHORT).show();
+            jetId.requestFocus();
+        }else{
+            Url+="delete.php";
+            StringRequest postRequest = new StringRequest(Request.Method.POST, Url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    limpiarCampos(v);
+                    Toast.makeText(getApplicationContext(), "¿Si se elimino? OBVIO BOBIS!", Toast.LENGTH_SHORT).show();
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(getApplicationContext(), "Nada, intente de nuevo o algo...", Toast.LENGTH_SHORT).show();
+                }
+            }){
+                protected Map<String, String> getParams(){
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("id", jetId.getText().toString().trim());
+                    return params;
+                }
+            };
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            requestQueue.add(postRequest);
+        }
+    }
+    public void sell(View v){
+        Id=jetId.getText().toString();
+        if (Id.isEmpty()){
+            Toast.makeText(getApplicationContext(), "Mijoooo... El ID ._.", Toast.LENGTH_SHORT).show();
+            jetId.requestFocus();
+        }else{
+            Url+="sell.php";
+
+            StringRequest postRequest = new StringRequest(Request.Method.POST, Url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    limpiarCampos(v);
+                    Toast.makeText(getApplicationContext(), "Melo, Ya lo vendió", Toast.LENGTH_SHORT).show();
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(getApplicationContext(), "No, viejo. Eso no funcionó", Toast.LENGTH_SHORT).show();
+                }
+            }){
+                protected Map<String, String> getParams(){
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("id",jetId.getText().toString().trim());
+                    return params;
+                }
+            };
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            requestQueue.add(postRequest);
+        }
+    }
+    public void limpiarCampos(View v){
+        jetId.setText("");
+        jetNombre.setText("");
+        jetValor.setText("");
+        jcbActivo.setChecked(false);
+        jetId.requestFocus();
     }
 }
